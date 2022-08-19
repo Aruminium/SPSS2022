c6be9ea5e145bc965d28134e8cd154b9a5def85d
Aruminium <b2201680@photon.chitose.ac.jp>
2022-08-20 04:00:51 +0900

    create cradUI for docs/index.html

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,9 @@
-$(document).on(() => {
-  $(".your-class").slick({
-    setting: setting-value
-  });
-});
+const card = (URL, imgSrc, title, desc) => {return(
+  <section class="card-list">
+    <a class="card-link" href=URL>
+      <figure class="card-figure"><img src=imgSrc></figure>
+      <h2 class="card-title">title</h2>
+      <p class="card-text-tax">desc</p>
+    </a>
+  </section>
+)};
